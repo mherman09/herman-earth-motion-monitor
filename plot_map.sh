0a6d2550366382cdc12c5f7b2d00da15692b55d6
Matt Herman <mherman2@csub.edu>
2025-03-14 13:55:45 -0700

    Fix spacing in event list on map

--- plot_map.sh
+++ plot_map.sh
@@ -73,45 +73,45 @@ do
     # Map frame
     gmt psbasemap $PROJ $LIMS -Bxg30 -Byg30 $SHFT -K -O --MAP_FRAME_PEN=0.5p --MAP_GRID_PEN=0.25p,105@40 >> $PSFILE
 
-    # Station locations
-    for TRACE in ../SAC/*.CI.*.sac ../SAC/*.IU.*.sac
-    do
+    # # Station locations
+    # for TRACE in ../SAC/*.CI.*.sac ../SAC/*.IU.*.sac
+    # do
 
-        echo getting station info for $TRACE >> $LOG_FILE
+    #     echo getting station info for $TRACE >> $LOG_FILE
 
-        # Get directory name and ASCII seismogram file name
-        DIR=`dirname $TRACE`
-        ASCII_FILE=`basename $TRACE .sac`.dat
+    #     # Get directory name and ASCII seismogram file name
+    #     DIR=`dirname $TRACE`
+    #     ASCII_FILE=`basename $TRACE .sac`.dat
 
-        # Plot station name
-        KSTNM=`saclhdr -KSTNM $TRACE`
-        STLO=`saclhdr -STLO $TRACE`
-        STLA=`saclhdr -STLA $TRACE`
-        DIST=`lola2distaz -c $LON0 $LAT0 $STLO $STLA | awk '{print $1/111.19}'`
-        PLOT=`echo $DIST | awk '{if($1<=90){print "Y"}else{print "N"}}'`
-        if [ $PLOT == "Y" ]
-        then
-            echo $STLO $STLA | gmt psxy $PROJ $LIMS -Si0.1i -W1p -Gred -N $SHFT -K -O >> $PSFILE
-            echo $STLO $STLA $KSTNM | gmt pstext $PROJ $LIMS -F+f7,1+jCB -D0/0.07i -N $SHFT -K -O >> $PSFILE
-        fi
-    done
+    #     # Plot station name
+    #     KSTNM=`saclhdr -KSTNM $TRACE`
+    #     STLO=`saclhdr -STLO $TRACE`
+    #     STLA=`saclhdr -STLA $TRACE`
+    #     DIST=`lola2distaz -c $LON0 $LAT0 $STLO $STLA | awk '{print $1/111.19}'`
+    #     PLOT=`echo $DIST | awk '{if($1<=90){print "Y"}else{print "N"}}'`
+    #     if [ $PLOT == "Y" ]
+    #     then
+    #         echo $STLO $STLA | gmt psxy $PROJ $LIMS -Si0.1i -W1p -Gred -N $SHFT -K -O >> $PSFILE
+    #         echo $STLO $STLA $KSTNM | gmt pstext $PROJ $LIMS -F+f7,1+jCB -D0/0.07i -N $SHFT -K -O >> $PSFILE
+    #     fi
+    # done
 
-    # Significant earthquakes
-    if [ "$USGS_EQ_QUERY_ERROR" == "N" ]
-    then
-        echo Earthquakes plotted on map centered at LON0=$LON0 LAT0=$LAT0
-        gmt makecpt -T0/100/10 -Cplasma -I -D > dep.cpt
-        awk '{print '$LON0','$LAT0',$3,$2}' ../sig_eq.tmp |\
-            lola2distaz -f stdin | awk '{print $1}' > dist.tmp
-        paste dist.tmp ../sig_eq.tmp |\
-            awk '{
-                if ($1/111.19<=90) {
-                    print $4,$3,$5,$6*$6*$6*0.0016
-                    printf("%10.3f%10.3f%8.2f%7.1f\n"),$4,$3,$5,$6 > "/dev/stderr"
-                }
-            }END{print "" > "/dev/stderr"}' |\
-            gmt psxy $PROJ $LIMS -Sci -W1p -Cdep.cpt $SHFT -N -K -O >> $PSFILE
-    fi
+    # # Significant earthquakes
+    # if [ "$USGS_EQ_QUERY_ERROR" == "N" ]
+    # then
+    #     echo Earthquakes plotted on map centered at LON0=$LON0 LAT0=$LAT0
+    #     gmt makecpt -T0/100/10 -Cplasma -I -D > dep.cpt
+    #     awk '{print '$LON0','$LAT0',$3,$2}' ../sig_eq.tmp |\
+    #         lola2distaz -f stdin | awk '{print $1}' > dist.tmp
+    #     paste dist.tmp ../sig_eq.tmp |\
+    #         awk '{
+    #             if ($1/111.19<=90) {
+    #                 print $4,$3,$5,$6*$6*$6*0.0016
+    #                 printf("%10.3f%10.3f%8.2f%7.1f\n"),$4,$3,$5,$6 > "/dev/stderr"
+    #             }
+    #         }END{print "" > "/dev/stderr"}' |\
+    #         gmt psxy $PROJ $LIMS -Sci -W1p -Cdep.cpt $SHFT -N -K -O >> $PSFILE
+    # fi
 done
 
 # Magnitude Legend
@@ -135,21 +135,24 @@ echo 0 1 Depth \(km\) | gmt pstext -JX1i -R0/1/0/1 -F+f28,1+jCT -Xa17.5i -Ya28.0
 gmt psscale -Dx17.5i/25.5i+w-2.8i/0.25i -Cdep.cpt -Ba20 -K -O --FONT_ANNOT=20 >> $PSFILE
 
 # Earthquake list
-SHFT="-Xa13.0i -Ya17.5i"
-echo 0 1 Event List | gmt pstext -JX1i -R0/1/0/1 -F+f28,1+jLT $SHFT -D0/0.7i -N -K -O >> $PSFILE
-echo 0 1 "Origin(UTC) Lat     Lon      Dep(km) Mag" |\
-    gmt pstext -JX1i -R0/1/0/1 -F+f20,8+jLT $SHFT -N -K -O >> $PSFILE
+PROJ=-JX7i/4i
+LIMS=-R0/7/0/4
+SHFT="-Xa13.0i -Ya15.5i"
+echo 0 4.0 Event List |\
+    gmt pstext $PROJ $LIMS -F+f28,1+jLT $SHFT -N -K -O >> $PSFILE
+echo 0 3.4 "Origin(UTC) Lat     Lon      Dep(km) Mag" |\
+    gmt pstext $PROJ $LIMS -F+f20,8+jLT $SHFT -N -K -O >> $PSFILE
 awk '{
     ot = substr($1,12,8)
     lo = $3
     la = $2
     dp = $4
     mg = $5
-    printf("%.1f  %.1f  %-11s\ %-8.2f%-8.2f %-8.2f%-7.1f\n"), 0,1-NR*0.45,ot,la,lo,dp,mg
-    printf("%.1f  %.1f  %-11s\ %-8.2f%-8.2f %-8.2f%-7.1f\n"), 0,1-NR*0.45,ot,la,lo,dp,mg > "/dev/stderr"
+    printf("%.1f  %.3f  %-11s\ %-8.2f%-8.2f %-8.2f%-7.1f\n"), 0,3.40-NR*0.35,ot,la,lo,dp,mg
 }' ../sig_eq.tmp |\
-    gmt pstext -JX1i -R0/1/0/1 -F+f20,8+jLT $SHFT -N -K -O >> $PSFILE
-
+    gmt pstext $PROJ $LIMS -F+f20,8+jLT $SHFT -N -K -O >> $PSFILE
+# gmt psbasemap $PROJ $LIMS -Bxg0.2 -Byg0.2 $SHFT -K -O --MAP_GRID_PEN=0.25p,105 >> $PSFILE
+# gmt psbasemap $PROJ $LIMS -Bxg1 -Byg1 $SHFT -K -O --MAP_GRID_PEN=1p >> $PSFILE
 
 # Finalize map
 gmt psxy -T -O >> $PSFILE
