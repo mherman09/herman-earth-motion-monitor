7a6027ff694306bbb74543c854b144c218f7df99
Matt Herman <mherman2@csub.edu>
2026-06-26 11:36:34 -0700

    Speed up animation plotting with gmt movie

--- plot_globe_animation.sh
+++ plot_globe_animation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
+# set -e
+
+
 
 #####
 #   INITIALIZE LOG FILE
@@ -35,6 +38,23 @@ test -f $WVFM_LOG_FILE || { echo "$0 [ERROR]: could not find waveform log file $
 
 
 
+# Determine date version
+DATE_VERSION=$(date --version > /dev/null 2>&1 && echo "gnu-date" || echo "bsd-date")
+
+if [ "$DATE_VERSION" == "bsd-date" ]
+then
+    echo "$SCRIPT [`print_time`]: using BSD date" | tee -a $LOG_FILE
+elif [ "$DATE_VERSION" == "gnu-date" ]
+then
+    echo "$SCRIPT [`print_time`]: using GNU date" | tee -a $LOG_FILE
+else
+    echo "$SCRIPT [ERROR]: could not figure out version of date" 1>&2
+    exit 1
+fi
+
+
+
+
 
 
 
@@ -52,6 +72,22 @@ CALENDAR_TIME_END=`grep "CALENDAR_TIME_END=" $WVFM_LOG_FILE | awk -F"=" '{print 
 CALENDAR_TIME_START_LOCAL=`grep "CALENDAR_TIME_START_LOCAL=" $WVFM_LOG_FILE | awk -F"=" '{print $2}'`
 CALENDAR_TIME_END_LOCAL=`grep "CALENDAR_TIME_END_LOCAL=" $WVFM_LOG_FILE | awk -F"=" '{print $2}'`
 TIME_ZONE_LOCAL=`grep "TIME_ZONE_LOCAL=" $WVFM_LOG_FILE | awk -F= '{print $2}'`
+
+EPOCH_TIME_END=$(grep "EPOCH_TIME_END=" $WVFM_LOG_FILE | awk -F"=" '{print $2}')
+EPOCH_TIME_EQ_DOWNLOAD_START=$(echo $EPOCH_TIME_END | awk '{print $1-31536000}')
+
+if [ "$DATE_VERSION" == "bsd-date" ]
+then
+    CALENDAR_TIME_EQ_DOWNLOAD_START=`date -u -r ${EPOCH_TIME_EQ_DOWNLOAD_START} "+%Y-%m-%dT%H:%M:%S"`
+    CALENDAR_TIME_EQ_DOWNLOAD_START_LOCAL=`date -r "${EPOCH_TIME_EQ_DOWNLOAD_START}" "+%Y-%m-%dT%H:%M:%S"`
+elif [ "$DATE_VERSION" == "gnu-date" ]
+then
+    CALENDAR_TIME_EQ_DOWNLOAD_START=`date -u -d "@${EPOCH_TIME_EQ_DOWNLOAD_START}" "+%Y-%m-%dT%H:%M:%S"`
+    CALENDAR_TIME_EQ_DOWNLOAD_START_LOCAL=`date -d "@${EPOCH_TIME_EQ_DOWNLOAD_START}" "+%Y-%m-%dT%H:%M:%S"`
+else
+    echo "$SCRIPT [ERROR]: could not figure out version of date" 1>&2
+    exit 1
+fi
 
 cat > j << EOF
 CALENDAR_TIME_START=$CALENDAR_TIME_START
@@ -73,9 +109,12 @@ then
     MINSIG=100
 fi
 echo "$SCRIPT [`print_time`]: minimum significance = $MINSIG" | tee -a $LOG_FILE
-COMCAT_URL="https://earthquake.usgs.gov/fdsnws/event/1/query?starttime=${CALENDAR_TIME_START}&endtime=${CALENDAR_TIME_END}&minsig=${MINSIG}&format=csv"
+COMCAT_URL="https://earthquake.usgs.gov/fdsnws/event/1/query?starttime=${CALENDAR_TIME_EQ_DOWNLOAD_START}&endtime=${CALENDAR_TIME_END}&minsig=${MINSIG}&format=csv"
 echo "$COMCAT_URL" >> $LOG_FILE
 curl "$COMCAT_URL" > query.csv 2>> $LOG_FILE
+COMCAT_URL="https://earthquake.usgs.gov/fdsnws/event/1/query?starttime=${CALENDAR_TIME_START}&endtime=${CALENDAR_TIME_END}&minsig=${MINSIG}&format=csv"
+echo "$COMCAT_URL" >> $LOG_FILE
+curl "$COMCAT_URL" > query_2.csv 2>> $LOG_FILE
 
 
 
@@ -94,18 +133,23 @@ fi
 if [ "$USGS_EQ_QUERY_ERROR" == "N" ]
 then
 
-    NSIG=$(wc query.csv | awk '{print $1}')
-    echo "$SCRIPT [`print_time`]: plotting $NSIG significant earthquakes" | tee -a $LOG_FILE
+    for F in query.csv query_2.csv
+    do
+
+        NSIG=$(wc $F | awk '{print $1}')
+        echo "$SCRIPT [`print_time`]: plotting $NSIG significant earthquakes" | tee -a $LOG_FILE
 
 
-    # Reverse order of events
-    awk '{line[NR]=$0}END{for(i=NR;i>1;i--){print line[i]}}' query.csv > j
-    mv j query.csv
+        # Reverse order of events
+        awk '{line[NR]=$0}END{for(i=NR;i>1;i--){print line[i]}}' $F > j
+        mv j $F
 
 
-    # Save significant earthquakes
-    echo "$SCRIPT [`print_time`]: saving significant earthquakes in sig_eq.tmp" | tee -a $LOG_FILE
-    awk -F, '{print $1,$2,$3,$4,$5}' query.csv > sig_eq.tmp
+        # Save significant earthquakes
+        echo "$SCRIPT [`print_time`]: saving significant earthquakes in $(basename $F .csv).tmp" | tee -a $LOG_FILE
+        awk -F, '{print $1,$2,$3,$4,$5}' $F > $(basename $F .csv).tmp
+
+    done
 
 fi
 
@@ -115,7 +159,7 @@ fi
 
 
 #####
-#   PLOT SIGNIFICANT EARTHQUAKES
+#   PLOT GLOBE FRAMES
 #####
 
 # Do all the work in the ANIMATION directory
@@ -125,239 +169,188 @@ cd ANIMATION
 
 
 
+# Remove globe/ directory if it exists
+MOVIE_NAME=globe
+test -d ${MOVIE_NAME} && rm -rf ${MOVIE_NAME}
+
+
+
 # GMT settings
 echo "$SCRIPT [`print_time`]: setting GMT parameters" | tee -a $LOG_FILE
-gmt set PS_MEDIA 100ix100i
 gmt set MAP_FRAME_TYPE plain
 
 
-
-# Loop over longitudes
-I=1
-for LON0 in `seq 0 10 360`
-do
-
-    echo "$SCRIPT [`print_time`]: working on frame centered on lon=$LON0" | tee -a $LOG_FILE
-
-    # Set frame name
-    IZERO=`echo $I | awk '{printf("%05d"),$1}'`
-    PSFILE=frame_$IZERO.ps
-
-    # Plot map panels
-    PROJ=-JA${LON0}/0/5i
-    LIMS=-Rg
-
-    # Initialize
-    gmt psxy -T -K > $PSFILE
-
-    # Coastline
-    gmt pscoast $PROJ $LIMS -Di -G255 -S149/181/192 -A0/0/1 $SHFT -K -O >> $PSFILE
-
-    # Map frame
-    gmt psbasemap $PROJ $LIMS -Bxg30 -Byg30 $SHFT -K -O --MAP_FRAME_PEN=0.5p --MAP_GRID_PEN=0.25p,105@40 >> $PSFILE
-
-    # Finalize
-    gmt psxy -T -O >> $PSFILE
-
-    # Convert
-    gmt psconvert $PSFILE -Tg -A
-
-    # Update index
-    I=`echo $I | awk '{print $1+1}'`
-
-done
+# Save station list absolute path
+STA_FILE_DIR=$(pwd)/../SAC
+ls $STA_FILE_DIR/*.CI.*.sac > ${STA_FILE_DIR}/sta_file_list.tmp
+ls $STA_FILE_DIR/*.IU.*.sac >> ${STA_FILE_DIR}/sta_file_list.tmp
 
 
-ffmpeg -loglevel warning -framerate 24 -y -i "frame_%05d.png" -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -vcodec libx264 -pix_fmt yuv420p out.mp4
-
-exit
-PSFILE=map.ps
-
-
-
-# Initialize map
-echo "$SCRIPT [`print_time`]: initializing PostScript file" | tee -a $LOG_FILE
-gmt psxy -T -K > $PSFILE
-
-
-
-# Labels
-echo "$SCRIPT [`print_time`]: plotting figure labels" | tee -a $LOG_FILE
-echo Earthquakes Around The World | gmt pstext -JX1i -R0/1/0/1 -F+f32,1+cTL -Xa0.5i -Ya28.0i -N -K -O >> $PSFILE
-echo $CALENDAR_TIME_START @%2%to@%% $CALENDAR_TIME_END \(UTC\)|\
-    gmt pstext -JX1i -R0/1/0/1 -F+f20,0+cTL -Xa0.5i -Ya27.35i -N -K -O >> $PSFILE
-echo $CALENDAR_TIME_START_LOCAL @%2%to@%% $CALENDAR_TIME_END_LOCAL \($TIME_ZONE_LOCAL\)|\
-    gmt pstext -JX1i -R0/1/0/1 -F+f20,0+cTL -Xa0.5i -Ya26.9i -N -K -O >> $PSFILE
+# Save earthquake file absolute path
+EQ_FILE=$(pwd)/../query.tmp
+EQ_FILE_2=$(pwd)/../query_2.tmp
 
 
 
 
-# Plot map panels
-echo "$SCRIPT [`print_time`]: plotting maps" | tee -a $LOG_FILE
-for COORD in -90,0,0,20 \
-             0,0,5.1,20 \
-             90,0,10.2,20 \
-             180,0,15.3,20 \
-             0,90,7.6,24.5 \
-             0,-90,7.6,15.5
-do
 
-    LON0=`echo $COORD | awk -F, '{print $1}'`
-    LAT0=`echo $COORD | awk -F, '{print $2}'`
-    XSHFT=`echo $COORD | awk -F, '{print $3}'`
-    YSHFT=`echo $COORD | awk -F, '{print $4}'`
-    SHFT="-Xa${XSHFT}i -Ya${YSHFT}i"
+# Main animation script
+echo "$SCRIPT [`print_time`]: creating main animation script main.sh" | tee -a $LOG_FILE
 
-    PROJ=-JA$LON0/$LAT0/5i
-    LIMS=-Rg
+EQ_SCALE=0.0010
+
+cat > main.bash << EOF
+
+
+gmt begin
+
+
+    # Set center longitude
+    LON0=\$(echo \$MOVIE_COL0 | awk '{print -\$1}')
 
 
     # Coastline
-    gmt pscoast $PROJ $LIMS -Di -G255 -S149/181/192 -A0/0/1 $SHFT -K -O >> $PSFILE
+    gmt coast -JG\${LON0}/0/11.2c -Rg -Di -G255 -S149/181/192 -A0/0/1 -X6.4c -Y0.25c
 
-    # Map frame
-    gmt psbasemap $PROJ $LIMS -Bxg30 -Byg30 $SHFT -K -O --MAP_FRAME_PEN=0.5p --MAP_GRID_PEN=0.25p,105@40 >> $PSFILE
 
-    # Grid labels
-    if [ $LAT0 == 0 ]
-    then
-        LON0_MINUS_60=`echo $LON0 | awk '{print $1-60}'`
-        LON0_PLUS_60=`echo $LON0 | awk '{print $1+60}'`
-        grid -x $LON0_MINUS_60 $LON0_PLUS_60 -dx 30 -y 0 0 -dy 30 |\
-            awk '{
-                if ($1>=0) {
-                    printf("%14.6e %14.6e %.0f\\260E\n"),$1,$2,$1
-                } else {
-                    printf("%14.6e %14.6e %.0f\\260W\n"),$1,$2,-$1
-                }
-            }' |\
-            gmt pstext $PROJ $LIMS -F+f5,2,95+jBL -D0.02i/0.02i -t40 $SHFT -K -O >> $PSFILE
-        grid -x $LON0 $LON0 -dx 30 -y -60 60 -dy 30 |\
-            awk '{
-                if ($2>=0) {
-                    printf("%14.6e %14.6e %.0f\\260N\n"),$1,$2,$2
-                } else {
-                    printf("%14.6e %14.6e %.0f\\260S\n"),$1,$2,-$2
-                }
-            }' |\
-            gmt pstext $PROJ $LIMS -F+f5,2,95+jTR -D-0.02i/-0.02i -t40 $SHFT -K -O >> $PSFILE
-    fi
+    # Basemap
+    gmt basemap -Bxg30 -Byg30 --MAP_FRAME_PEN=0.5p --MAP_GRID_PEN=0.25p,105@70
 
-    # Station locations
-    echo "$SCRIPT [`print_time`]: plotting seismic stations shown in waveforms.png" | tee -a $LOG_FILE
-    for TRACE in ../SAC/*.CI.*.sac ../SAC/*.IU.*.sac
-    do
-
-        # Get directory name and ASCII seismogram file name
-        DIR=`dirname $TRACE`
-        ASCII_FILE=`basename $TRACE .sac`.dat
-
-        # Plot station name
-        KSTNM=`saclhdr -KSTNM $TRACE`
-        STLO=`saclhdr -STLO $TRACE`
-        STLA=`saclhdr -STLA $TRACE`
-        DIST=`lola2distaz -c $LON0 $LAT0 $STLO $STLA | awk '{print $1/111.19}'`
-        PLOT=`echo $DIST | awk '{if($1<=90){print "Y"}else{print "N"}}'`
-        if [ $PLOT == "Y" ]
-        then
-            echo $STLO $STLA | gmt psxy $PROJ $LIMS -Si0.1i -W1p -Gred -N $SHFT -K -O >> $PSFILE
-            echo $STLO $STLA $KSTNM | gmt pstext $PROJ $LIMS -F+f7,1+jCB -D0/0.07i -N $SHFT -K -O >> $PSFILE
-        fi
-    done
 
     # Significant earthquakes
     if [ "$USGS_EQ_QUERY_ERROR" == "N" ]
     then
-        echo "$SCRIPT [`print_time`]: center coordinates: LON0=$LON0 LAT0=$LAT0" | tee -a $LOG_FILE
-        gmt makecpt -T0/100/10 -Cplasma -I -D > dep.cpt
-        awk '{print '$LON0','$LAT0',$3,$2}' ../sig_eq.tmp |\
-            lola2distaz -f stdin | awk '{print $1}' > dist.tmp
-        paste dist.tmp ../sig_eq.tmp |\
+        gmt makecpt -T0/100/10 -Cplasma -I -D
+        awk '{print '\$LON0',0,\$3,\$2}' ${EQ_FILE} |\\
+            /home/mherman2/Research/Hdef/bin/lola2distaz -f stdin |\\
+            awk '{print \$1}' |\\
+            paste - ${EQ_FILE} |\\
             awk '{
-                if ($1/111.19<=90) {
-                    print $4,$3,$5,$6*$6*$6*0.0016
-                    printf("%10.3f%10.3f%8.2f%7.1f\n"),$4,$3,$5,$6 > "/dev/stderr"
+                if (\$1/111.19<=93 && \$6>=5.0) {
+                    if (\$6<7.0) {
+                        p=1
+                    } else if (\$6<8.0) {
+                        p=2
+                    } else {
+                        p=3
+                    }
+                    if (\$1/111.19>90) {
+                        t = (\$1/111.19-90)*33
+                    } else {
+                        t = 0
+                    }
+                    print "> -W"p"p"
+                    print \$4,\$3,\$5,\$6*\$6*\$6*$EQ_SCALE,t
                 }
-            }' |\
-            gmt psxy $PROJ $LIMS -Sci -W1p -Cdep.cpt $SHFT -N -K -O >> $PSFILE
+            }' |\\
+            gmt plot -Sci -W1p -C -N -t
+        awk '{print '\$LON0',0,\$3,\$2}' ${EQ_FILE_2} |\\
+            /home/mherman2/Research/Hdef/bin/lola2distaz -f stdin |\\
+            awk '{print \$1}' |\\
+            paste - ${EQ_FILE} |\\
+            awk '{if (\$1/111.19<=93 && \$6>=5.0) {if(\$6<7.0){p=1}else if(\$6<8.0){p=2}else{p=3};print "> -W"p"p,green"; print \$4,\$3,\$5,\$6*\$6*\$6*$EQ_SCALE}}' |\\
+            gmt plot -Sci -W1p -C -N
     fi
 
-done
+
+    # Seismic stations
+    for TRACE in \$(cat ${STA_FILE_DIR}/sta_file_list.tmp)
+    do
+
+        # Get directory name and ASCII seismogram file name
+        DIR=\$(dirname \$TRACE)
+        ASCII_FILE=\$(basename \$TRACE .sac).dat
+
+        # Plot stations
+        KSTNM=\$(/home/mherman2/Research/PROGRAMS.330/bin/saclhdr -KSTNM \$TRACE)
+        STLO=\$(/home/mherman2/Research/PROGRAMS.330/bin/saclhdr -STLO \$TRACE)
+        STLA=\$(/home/mherman2/Research/PROGRAMS.330/bin/saclhdr -STLA \$TRACE)
+        DIST=\$(/home/mherman2/Research/Hdef/bin/lola2distaz -c \$LON0 0 \$STLO \$STLA |\\
+            awk '{print \$1/111.19}')
+        PLOT=\$(echo \$DIST | awk '{if(\$1<=93){print "Y"}else{print "N"}}')
+        if [ \$PLOT == "Y" ]
+        then
+            echo \$STLO \$STLA | gmt plot -Si0.1i -W1p -Gred -N
+            echo \$STLO \$STLA \$KSTNM | gmt text -F+f7,1+jCB -D0/0.07i -N
+        fi
+    done
 
 
+    # Magnitude legend
+    X0=14.0
+    Y0=2.5
+    echo \$X0 8.00 Magnitude | gmt pstext -JX1c -R0/1/0/1 -F+f20,2+jCT -N -D0/\${Y0}
+    echo 6.10 9.0 > mag_legend.tmp
+    echo 4.30 8.0 >> mag_legend.tmp
+    echo 3.00 7.0 >> mag_legend.tmp
+    echo 2.10 6.0 >> mag_legend.tmp
+    echo 1.50 5.0 >> mag_legend.tmp
+    awk '{print '\$X0', \$1+'\$Y0', \$2^3*$EQ_SCALE}' mag_legend.tmp | gmt plot -W1p -G235 -Sci -N
+    awk '{print '\$X0'+\$2^3*$EQ_SCALE/2*2.54, \$1+'\$Y0', 9+(\$2-4)*2.0, \$2}' mag_legend.tmp |\\
+        gmt text -F+f+jLM -D0.05i/0 -N
 
-# Magnitude Legend
-echo "$SCRIPT [`print_time`]: plotting magnitude legend" | tee -a $LOG_FILE
-echo 0 1 Magnitude | gmt pstext -JX1i -R0/1/0/1 -F+f28,1+jCT -Xa14.5i -Ya28.0i -D0.15i/0 -N -K -O >> $PSFILE
-cat > seis_scale.tmp << EOF
-2.20 9.0
-1.38 8.0
-0.76 7.0
-0.37 6.0
-0.14 5.0
-0.00 4.0
+
+    # Depth Legend
+    echo -2.90 10.5 Depth \(km\) |\\
+        gmt text -F+f20,2+jCT -N
+    gmt colorbar -Dx-3.0/2.5+w-6.8/0.60+ef+ma -C --FONT_ANNOT=14
+
+
+    # Seismometer legend
+    echo \$X0 2.60 Seismometers | gmt pstext -JX1c -R0/1/0/1 -F+f20,2+jCT -N
+    echo \$X0 1.50 Station | gmt text -F+f7,1+jCB -D0/0.07i -N
+    echo \$X0 1.50 | gmt plot -Si0.1i -W1p -Gred -N
+
+
+gmt end
+
 EOF
-SHFT="-Xa14.5i -Ya25.6i"
-awk '{print 0.0,$1,$2^3*0.0016}' seis_scale.tmp |\
-    gmt psxy -JX1i -R0/1/0/1 -W1p -G235 -Sci $SHFT -N -K -O >> $PSFILE
-awk '{print 0.0+$2^3*0.0016/2,$1,11+($2-4)*1.5,$2}' seis_scale.tmp |\
-    gmt pstext -J -R -F+f+jLM -D0.05i/0 $SHFT -N -K -O >> $PSFILE
 
 
 
-# Depth Legend
-echo "$SCRIPT [`print_time`]: plotting depth legend" | tee -a $LOG_FILE
-echo 0 1 Depth \(km\) | gmt pstext -JX1i -R0/1/0/1 -F+f28,1+jCT -Xa17.5i -Ya28.0i -D0.15i/0 -N -K -O >> $PSFILE
-gmt psscale -Dx17.5i/25.5i+w-2.8i/0.25i -Cdep.cpt -Ba20 -K -O --FONT_ANNOT=20 >> $PSFILE
+# Run gmt movie
+echo "$SCRIPT [`print_time`]: running gmt movie to generate animation" | tee -a $LOG_FILE
 
+LON_START=0
+LON_END=360
+DLON=0.5
+DLON=90
+LON_END=$(echo $LON_END $DLON | awk '{print $1-$2*2}')
 
+FPS=24
+FPS=12
 
-# Earthquake list
-echo "$SCRIPT [`print_time`]: plotting text list of earthquakes on map" | tee -a $LOG_FILE
-PROJ=-JX7i/4i
-LIMS=-R0/7/0/4
-SHFT="-Xa13.0i -Ya15.5i"
-echo 0 4.0 Event List |\
-    gmt pstext $PROJ $LIMS -F+f28,1+jLT $SHFT -N -K -O >> $PSFILE
-echo 0 3.4 "Origin(UTC) Latitude Longitude Depth(km) Magnitude" |\
-    gmt pstext $PROJ $LIMS -F+f16,8+jLT $SHFT -N -K -O >> $PSFILE
-NEQ=`wc ../sig_eq.tmp | awk '{print $1}'`
-awk '{
-    ot = substr($1,12,8)
-    lo = $3
-    la = $2
-    dp = $4
-    mg = $5
-    if (NR<=8) {
-        printf("%.1f  %.3f  %-11s\ %-9.2f%-9.2f %-10.2f%-7.1f\n"), 0,3.40-NR*0.32,ot,la,lo,dp,mg
-    }
-}END{
-    if(NR>8){
-        printf("%.1f  %.3f  :           :        :         :         :\n"), 0,3.40- 9*0.32
-        printf("%.1f  %.3f  %d total earthquakes plotted\n"), 0,3.40-10*0.32,'$NEQ'
-    }
-}' ../sig_eq.tmp |\
-    gmt pstext $PROJ $LIMS -F+f16,8+jLT $SHFT -N -K -O >> $PSFILE
-# gmt psbasemap $PROJ $LIMS -Bxg0.2 -Byg0.2 $SHFT -K -O --MAP_GRID_PEN=0.25p,105 >> $PSFILE
-# gmt psbasemap $PROJ $LIMS -Bxg1 -Byg1 $SHFT -K -O --MAP_GRID_PEN=1p >> $PSFILE
+CANVAS_WID=24c
+CANVAS_HGT=12.5c
+CANVAS_DPU=80
 
+MASTER_FRAME=3
 
+# -Zs removes all directories, temporary scripts, etc.
+# -L adds labels
+# -Pb+Pblack+pwhite+a adds a progress circle
+# -x8 limits number of cores
 
-# Finalize map
-echo "$SCRIPT [`print_time`]: finalizing PostScript file" | tee -a $LOG_FILE
-gmt psxy -T -O >> $PSFILE
-
-
-
-echo "$SCRIPT [`print_time`]: converting to PNG" | tee -a $LOG_FILE
-gmt psconvert $PSFILE -Tg -A -I+m0.1i -E300
+gmt movie main.bash \
+    -N${MOVIE_NAME} \
+    -C${CANVAS_WID}x${CANVAS_HGT}x${CANVAS_DPU} \
+    -T${LON_START}/${LON_END}/${DLON} \
+    -M${MASTER_FRAME},png \
+    -Fgif+l \
+    -Zs \
+    -Gwhite \
+    -D${FPS} \
+    -Ls"Earthquakes Around the World (Past Year)"+jTC+f24,0 \
+    -Ls"$CALENDAR_TIME_EQ_DOWNLOAD_START @%2%@:8:to@::@%% $CALENDAR_TIME_END \(UTC\)"+jBL+f9,2 \
+    -Ls"$CALENDAR_TIME_EQ_DOWNLOAD_START_LOCAL @%2%@:8:to@::@%% $CALENDAR_TIME_END_LOCAL \($TIME_ZONE_LOCAL\)"+jBR+f9,2 \
+    -Vi
 
 
 
 echo "$SCRIPT [`print_time`]: cleaning up" | tee -a $LOG_FILE
-mv map.png ..
-rm $PSFILE
-
-cd ..
 
 echo "$SCRIPT [`print_time`]: finished" | tee -a $LOG_FILE
+
+cd ..
+# pkill vlc
+# vlc ANIMATION/${MOVIE_NAME}.mp4 &
